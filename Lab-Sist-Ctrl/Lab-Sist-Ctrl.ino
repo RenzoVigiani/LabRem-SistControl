2c38c5f506b8554f0b6b339515b34bac8d1cdf6f
RenzoVigiani <vigiani.renzo@gmail.com>
2022-08-18 14:00:11 -0300

    Modificaciones de protocolo en readme. Revisar

--- Lab-Sist-Ctrl/Lab-Sist-Ctrl.ino
+++ Lab-Sist-Ctrl/Lab-Sist-Ctrl.ino
@@ -11,7 +11,7 @@ EthernetServer server = EthernetServer(server_port);
 #define const_mje 256
 #define const_valores 236
 
-//char Mensaje_recibido[const_mje] = {0}; // Mensaje recibido por ethernet. (Comando + JSON) 
+//char Mensaje_recibido[const_mje] = {0}; // Mensaje recibido por ethernet. (Comando + JSON)
 //char valores_recibidos[const_valores] = {0}; // JSON recibido.
 //----------------------------------------------//
 //------- Defino mensajes de error predeterminado
@@ -25,123 +25,98 @@ int Errores = 0;
 //------- Defino variables globales
   #define Numero_laboratorio 0
 // Nombres para los pines GPIO
-// Cyclone II
-  #define Pulsador_II_0 6
-  #define Pulsador_II_1 7
-  #define Pulsador_II_2 8
-  #define Pulsador_II_3 9
+  // Bomba de agua
+  #define Pin_Bomba 9
+  //  Habilitadores
+  #define Pin_Agua 8
+  #define Pin_Luz 7
   // Leds
-  #define Led_II_0 10 // Indicador 0
-  #define Led_II_1 11 // Indicador 1
-  #define Led_II_2 12 // Indicador 2
-  #define Led_II_3 13 // Indicador 3
-// Cyclone IV
-  #define Pulsador_IV_0 30
-  #define Pulsador_IV_1 31
-  #define Pulsador_IV_2 32
-  #define Pulsador_IV_3 33
-  // Leds
-  #define Led_IV_0 34 // Indicador 0
-  #define Led_IV_1 35 // Indicador 1
-  #define Led_IV_2 36 // Indicador 2
-  #define Led_IV_3 37 // Indicador 3
+  #define Led_0 10 // Indicador 0
+  #define Led_1 11 // Indicador 1
   // Manejo del serial
   #define velocidad_serial_default 9600
 
 //////// VAriables Locales de json //////////////
 // Estado
   int num_Lab=Numero_laboratorio;
-  bool subLab=0;// False = CycloneII / True= CycloneIV
-  bool iniLab=false;
+  bool subLab=0;// false = Aliassing / true= Control de posición
+  bool iniLab=false; // false = desactivado / true= activado
 // Velocidad
   uint32_t velocidad_serial=9600;
-// Pulsadores
-  bool pulsador_0=false;
-  bool pulsador_1=false;
-  bool pulsador_2=false;
-  bool pulsador_3=false;
+// Llaves
+  bool llave_0=false;
+  bool llave_1=false;
+//Analógicos
+  int  analogico_0=0;
+  int  analogico_1=0;
+  int  analogico_2=0;
+
 // UART
   char* input_ether_char={0};
-  String input_ether_string="Holis rx"; // variable que se usa para guardar el mje recibido de ethernet. 
-  String input_fpga_string=""; //  variable que se usa para guardar el mje recibido de fpga.
+  String input_ether_string="Holis rx"; // variable que se usa para guardar el mje recibido de ethernet.
+  String input_ctrl_string=""; //  variable que se usa para guardar el mje recibido del arduino del Lab de control de posición.
 //--- Variables auxiliares ---//
-// Indicadores
-  bool indicador_0=false;
-  bool indicador_1=false;
-  bool indicador_2=false;
-  bool indicador_3=false;
 // banderas
-  bool bandera_rep=0; // bandera para limitar la cantidad de repeticiones de mensaje de lab incorrecto 
-  bool sentido=0;
-  int repeticiones=0; // contador para repeticiones.
+  // bool bandera_rep=0; // bandera para limitar la cantidad de repeticiones de mensaje de lab incorrecto
+  // bool sentido=0;
+  // int repeticiones=0; // contador para repeticiones.
 
 //--- FUNCIONES --- //
   void asignarPines(int pul_0,int pul_1,int pul_2,int pul_3,int led_0,int led_1,int led_2,int led_3);
   void Comunicacion(bool Serial_out);
   void Control();
-  
+
 //----------------------------------------------//
 // Realizo las configuraciones iniciales
 void setup() {
   uint8_t myMAC[6] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED}; // defino mac del dispositivo.
-  IPAddress myIP(192,168,1,108); // 172.20.5.140 Defino IP Address del dispositivo. 
-  Serial.begin(velocidad_serial_default); // Inicializo Puerto serial 0 
-  Serial1.begin(velocidad_serial_default); // Inicializo Puerto serial 1 - CII - GPIO 19 tx 18 rx
-  Serial2.begin(velocidad_serial_default); // Inicializo Puerto serial 2 - CIV - GPIO 16 tx 17 rx
-  while (!Serial) continue; 
+  IPAddress myIP(192,168,1,108); // 172.20.5.140 Defino IP Address del dispositivo.
+  Serial.begin(velocidad_serial_default); // Inicializo Puerto serial 0
+  Serial1.begin(velocidad_serial_default); // Inicializo Puerto serial 1 - Arduino Ctrl de posición - GPIO 19 tx 18 rx
+  while (!Serial) continue;
   Ethernet.begin(myMAC,myIP);  // Inicializo libreria Ethernet
   server.begin(); // Start to listen
   Serial.println(F("Server is ready."));
   Serial.print(F("Please connect to http://"));
   Serial.println(Ethernet.localIP());
   Serial.println("Port:" + (String)server_port);
-// ------- Defino GPIO MODE (INPUT/OUTPUT)--------  //  
-  input_fpga_string.reserve(200);
-//Cyclone II
-  pinMode(Led_II_0,INPUT);
-  pinMode(Led_II_1,INPUT);
-  pinMode(Led_II_2,INPUT);
-  pinMode(Led_II_3,INPUT);
-  pinMode(Pulsador_II_0,OUTPUT);
-  pinMode(Pulsador_II_1,OUTPUT);
-  pinMode(Pulsador_II_2,OUTPUT);
-  pinMode(Pulsador_II_3,OUTPUT);
-//Cyclone IV
-  pinMode(Led_IV_0,INPUT);
-  pinMode(Led_IV_1,INPUT);
-  pinMode(Led_IV_2,INPUT);
-  pinMode(Led_IV_3,INPUT);
-  pinMode(Pulsador_IV_0,OUTPUT);
-  pinMode(Pulsador_IV_1,OUTPUT);
-  pinMode(Pulsador_IV_2,OUTPUT);
-  pinMode(Pulsador_IV_3,OUTPUT);
+// ------- Defino GPIO MODE (INPUT/OUTPUT)--------  //
+  input_ctrl_string.reserve(200);
+  pinMode(Led_0,OUTPUT);
+  pinMode(Led_1,OUTPUT);
+  pinMode(Pin_Agua,OUTPUT);
+  pinMode(Pin_Bomba,OUTPUT);
+  pinMode(Pin_Luz,OUTPUT); 
   //------ Definir estados iniciales ------//
 }
 
-void loop(){  
+void loop(){
   //////////// Strings de comunicación /////////////
-  char Mensaje_recibido[const_mje] = {0}; // Mensaje recibido por ethernet. (Comando + JSON) 
+  char Mensaje_recibido[const_mje] = {0}; // Mensaje recibido por ethernet. (Comando + JSON)
   char valores_recibidos[const_valores] = {0}; // JSON recibido.  // Wait for an incomming connection
-  EthernetClient client = server.available(); 
+  EthernetClient client = server.available();
   if(client){ // Si tengo un cliente conectado
-    while (client.available()){ 
+    while (client.available()){
       client.readBytesUntil('\r', Mensaje_recibido, sizeof(Mensaje_recibido)); // Tomo el mensaje recibido.
-      strncpy(valores_recibidos,&Mensaje_recibido[15],(sizeof(Mensaje_recibido)-15)); 
+      strncpy(valores_recibidos,&Mensaje_recibido[15],(sizeof(Mensaje_recibido)-15));
       Serial.print("Mensaje Recibido: " + (String)Mensaje_recibido);
       //----------- GET ----------- //
       if (strstr(Mensaje_recibido, "GET /HTTP/1.1") != NULL) { // Compruebo si llega un GET, respondo valores actuales
-        StaticJsonDocument<256> doc;     
+        StaticJsonDocument<256> doc;
         JsonArray Estado = doc.createNestedArray("Estado");
           Estado.add(num_Lab);
           Estado.add(subLab);
-          Estado.add(iniLab);        
-        JsonArray Indicadores = doc.createNestedArray("Indicadores");
-          Indicadores.add(indicador_0);
-          Indicadores.add(indicador_1);
-          Indicadores.add(indicador_2);
-          Indicadores.add(indicador_3);
+          Estado.add(iniLab);
+        JsonArray Llaves = doc.createNestedArray("Llaves");
+          Llaves.add(llave_0);
+          Llaves.add(llave_1);
+        JsonArray Analogicos = doc.createNestedArray("Analogicos");
+          Analogicos.add(analogico_0);
+          Analogicos.add(analogico_1);
+          Analogicos.add(analogico_2);        
         doc["velocidad"] = velocidad_serial;
-        doc["Serial"] = input_fpga_string;
+        doc["Posicion"] = input_ctrl_string;
+        
         doc["Error"] = Errores;
 
         Serial.print(F("Sending: "));
@@ -157,11 +132,11 @@ void loop(){
   // Write JSON document
         serializeJsonPretty(doc, client);
       }
-      //------------ POST ----------//      
+      //------------ POST ----------//
       if (strstr(Mensaje_recibido, "POST /HTTP/1.1") !=NULL) { // Compruebo si llega un POST y respondo, habilito banderas.
         Errores=0;
-        if(bandera_rep==1)bandera_rep=0;//reinicio bandera de repetición cuando tengo un mje nuevo.
-        Serial.println("Solicitud de escritura recibida");        
+//        if(bandera_rep==1)bandera_rep=0;//reinicio bandera de repetición cuando tengo un mje nuevo.
+        Serial.println("Solicitud de escritura recibida");
         client.println();
         client.println(F("HTTP/1.1 200 OK"));
         client.println();
@@ -177,7 +152,7 @@ void loop(){
           subLab = Estado[1]; // 0 [Sub Lab 1 con Cyclone II], 1 [SubLab 2 con Cyclone IV]
           iniLab = Estado[2]; // 1 [Inicia Experimento], 0 [Finaliza Experimento]
         if(num_Lab==Numero_laboratorio){ // Si es el numero de laboratorio asigno variables recibidas.
-          velocidad_serial = doc["velocidad"];       
+          velocidad_serial = doc["velocidad"];
           JsonArray Pulsadores = doc["Pulsadores"];
             pulsador_0 = Pulsadores[0];
             pulsador_1 = Pulsadores[1];
@@ -188,7 +163,7 @@ void loop(){
         }
       }
     }
-  }  
+  }
   else{ // Si no está el cliente enviando algo, sigo haciendo lo que corresponde.
     Control();
   }
@@ -196,22 +171,22 @@ void loop(){
 
 /**
  * @brief Funcion utilizada para el control de laboratorios. "Se analiza las variables de entrada y se elije la placa a utilizar"
- * 
+ *
  */
 void Control(){
   if(num_Lab==Numero_laboratorio and bandera_rep==0){ // Si es el lab correcto - Control de numero de sublab.
     if (subLab and iniLab){ // UART
-      Serial.println("Sub-Lab: UART"); 
+      Serial.println("Sub-Lab: UART");
       Serial1.begin(velocidad_serial); // asigno la velocidad al serial 1
       asignarPines(Pulsador_II_0,Pulsador_II_1,Pulsador_II_2,Pulsador_II_3,Led_II_0,Led_II_1,Led_II_2,Led_II_3);
-      Serial1.print(input_ether_string);      
+      Serial1.print(input_ether_string);
       bandera_rep=1;
     }
     else if (!subLab and iniLab){ // I2C
-      Serial.println("Sub-Lab: I2C");  
+      Serial.println("Sub-Lab: I2C");
       Serial2.begin(velocidad_serial); // asigno la velocidad al serial 2
       asignarPines(Pulsador_IV_0,Pulsador_IV_1,Pulsador_IV_2,Pulsador_IV_3,Led_IV_0,Led_IV_1,Led_IV_2,Led_IV_3);
-      Serial2.print(input_ether_string);      
+      Serial2.print(input_ether_string);
       bandera_rep=1;
     }
     else{ // Laboratorio parado.
@@ -224,18 +199,18 @@ void Control(){
   }
   else{ // Laboratorio incorrecto
     if(bandera_rep==0){
-      Serial.println("Laboratorio incorrecto");  
-      bandera_rep = 1; 
+      Serial.println("Laboratorio incorrecto");
+      bandera_rep = 1;
       Errores=2;
     }
   }
   if(Serial1.available()){ //Controlo lo que se envía por el serial 1.
-    input_fpga_string=Serial1.readString();
-    Serial.println("Se recibio de serial 1: " + input_fpga_string);      
+    input_ctrl_string=Serial1.readString();
+    Serial.println("Se recibio de serial 1: " + input_ctrl_string);
   }
   else if(Serial2.available()){ //Controlo lo que se envía por el serial 1.
-    input_fpga_string=Serial2.readString();
-    Serial.println("Se recibio de serial 2: " + input_fpga_string);      
+    input_ctrl_string=Serial2.readString();
+    Serial.println("Se recibio de serial 2: " + input_ctrl_string);
   }
   if(repeticiones<10){// Se busca que no se repita mas de 10 veces una acción.
     repeticiones++;
@@ -248,7 +223,7 @@ void Control(){
 
 /**
  * @brief Selecciona las salidas y entradas en base a la placa elegida
- * 
+ *
  * @param pul_0 - Pulsador 0
  * @param pul_1 - Pulsador 1
  * @param pul_2 - Pulsador 2
--- README.md
+++ README.md
@@ -5,32 +5,72 @@ Cada vez que se quiere modificar algun dato en el laboratorio. El Servidor debe 
 POST /HTTP/1.1 + <JSON a enviar>
 ```
 
-##### Json a enviar (ejemplo)
+#### <ins>**Json a enviar (ejemplo)**</ins>
+_Sub-Lab: Submuestreo, aliasing y efecto estroboscópico_
 ```Ruby 
-{"Estado": [ 0, true, false], "Llaves": [ false, false ], "Analogico": [ 100, 200 ]}
+{"Estado": [ 1, false, false], "Habilitadores": [ false, false ], "Frecuencias": [ 0, 0 ]}
 ```
+>Donde:
+> - **Habilitadores** es un array de dos variables booleanas.
+>``` Habilitadores:[Habilitacion de agua, Habilitacion de luz]```
+> - **Frecuencia** es un array de dos variables enteras de 32bit que indican la frecuencias de agua y luz. 
+> ```Frecuencias:[Frecuencia de agua , Frecuencia de luz]```
+
+_SubLab: Control automatico de posición_
+```Ruby 
+{"Estado": [ 1, true, false], "Exitacion": [ 0, 0 ], "Regulacion": [ 0, 0, 0 ], "Planta":[0,0,0,0,1,1,1,2]}
+```
+>Donde:
+> - **Exitación** es un array de dos variables enteras de 32 bits que indican el tipo de exitación y su frecuencia.
+>    ```"Exitacion":[Tipo, frecuencia] ```
+> - **Regulación** es un array de tres variables enteras de 32 bits que indican el tipo de regulación. ```"Regulacion":[P,I,D] ```
+> - **Planta** es un array de ocho variables de 32 bits que indican los polos, ceros y la constante k de la planta a utilizar.
+> ```"Planta":[Polo_0, Polo_1, Polo_2, Polo_3, Cero_0, Cero_1, Cero_2, k] ```
+
 Y para obtener avances de los datos o reporte de los datos debe enviar un:
 ```Ruby 
 GET /HTTP/1.1
 ```
 De esta forma el Arduino responde ante una petición.
 
-##### Json a recibir (ejemplo)
-
+#### <ins>**Json a recibir (ejemplo)**</ins>
+_Sub-Lab: Submuestreo, aliasing y efecto estroboscópico_
 ```Ruby 
-{"Estado": [ 0, true, false], "Llaves": [ false, false ], "Analogico": [ 100, 200], "Error": 0}
+{"Estado": [ 0, false, true], "Habilitadores": [ false, false ], "Frecuencias": [ 0, 0 ], "Errores":0 }
 ```
+> Cada no de las variables indica el estado actual.
+
+_SubLab: Control automatico de posición_
+```Ruby 
+{"Estado":[2,true,true],"Posicion":[0], "Velocidad":[0], "Tiempo":[0],"Exitacion":[ 0 , 10 ]}
+```
+>Donde: 
+> - **Exitación:** Señal y frecuencia de exitación utilizada. Es la misma para un experimento en concreto.
+>   - *Tipo:*
+>     - 0: Escalón
+>     - 1: Cuadrada
+>     - 2: Senoidal
+>     - 3: triangular
+>   - *Frecuencia:* puede ser un numero entre 0 y 1000 expresado en Hz.
+> - Cada una de las siguientes variables son arrays de hasta 200 valores cada uno.
+>   - **Tiempo:** Se refiere al tiempo que dura el experimento. Se establece un tiempo de default (60 s o 120 s). se expresa en segundos (s).
+>   - **Velocidad:** Velocidad angular obtenida. Se mide en: [rad/s]
+>   - **Posición:** Posición obtenida. Se mide en [m]
+> 
+> **Estos datos deben ser guardados en una tabla para luego ser graficados.**   
+
 ## Sintaxis
 
 **Estado:**
-Es un array conformado por 3 elementos en el siguiente orden: [Laboratorio, SubLab, Inicio del experimento]
+Es un array conformado por 3 elementos en el siguiente orden: ``[Laboratorio, SubLab, Inicio del experimento]``
 
-|Laboratorio  | Sub Laboratorio  | Inicio del experimento | Laboratorio Seleccionado | Estado del experimento|
-|-|-----|-----|---------------------------------------------|--------|
-|1|true |true |Submuestreo, aliasing y efecto estroboscópico|Inicia  |
-|1|true |false|Submuestreo, aliasing y efecto estroboscópico|Finaliza|
-|1|false|true |Control automatico de posición               |Inicia  |
-|1|false|false|Control automatico de posición               |Finaliza|
+>Donde:
+> - **Laboratorio:** es el numero de laboratorio. En este caso ``1``
+> - **SubLab:** es el sub laboratorio seleccionado. En este caso
+  ``false = SubLab1 y true = SubLab2``
+>   - SubLab1= _``Sub-Lab: Submuestreo, aliasing y efecto estroboscópico``_
+>   - SubLab2= _``SubLab: Control automatico de posición``_
+> - **Inicio del experimento** es una variable booleana que indica el inicio del experimento. ``false = parado y true = iniciado``
 
 **Error:**
 Es una variable numerica que representa un mensaje de error.
@@ -42,22 +82,8 @@ Es una variable numerica que representa un mensaje de error.
 | Error 2       |    2    |
 | Error 3       |    3    |
 
-## Elementos por Laboratorio
 
-***Submuestreo, aliasing y efecto estroboscópico***
-
-- Llaves:  [habilitacion_agua,habilitacion_luz]
-- Analogico: [frec_agua,frec_luz]
-  - Donde:
-    - frec_agua es la frecuencia de oscilación.
-    - frec_luz es la frecuencia de parpadeo.
-
-***Control automatico de posición***
-
-- Llaves: [habilitacion_regulacion]
-- Analogico: [exitacion,frecuencia,regulacion]
-
-## **Interno Lab de control de posición**
+## **Protocolo Interno Lab de control de posición**
 Cada vez que se quiere modificar algun dato en el dispositivo de control de posición. El Servidor debe enviar un:
 ```Ruby 
 WRITE + <JSON a enviar>
@@ -89,11 +115,16 @@ WRITE + <JSON a enviar>
 >     - 2: Senoidal
 >     - 3: triangular
 >   - *Frecuencia:* puede ser un numero entre 0 y 1000 expresado en Hz.
->- **Errores:**
->   - 0: Sin errores
->   - 1: Error 1
->   - 2: Error 2
->   - 3: Error 3
+> - **Errores:**
+> Es una variable numerica que representa un mensaje de error.
+> 
+> | Tipo de error |  Valor  |
+> | ------------- | ------- |
+> | Sin errores   |    0    |
+> | Error 1       |    1    |
+> | Error 2       |    2    |
+> | Error 3       |    3    |
+
 
 En cuanto a la obtención de los datos, el arduino está testeando constantemente el puerto serie y va almacenando los datos recibidos en una memoria para enviar por bloques al servidor.
 Desde el dispositivo de control de posición se recibe.
